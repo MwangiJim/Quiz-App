ccc8dd19076ccda2faa0db2229024ea4cf0821ee
MwangiJim <55576765+MwangiJim@users.noreply.github.com>
2021-11-15 10:58:08 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,43 @@
+let questions = 
+[
+    {
+       question:"Which is the Capital city of Kenya?",
+       answer:"Nairobi",
+       options:[
+           "Dodoma",
+           "Durban",
+           "Nairobi",
+           "Kampala"
+       ]
+    },
+    {
+        question:"When did Kenya gain independence",
+        answer:"1963",
+        options:[
+            "1990",
+            "1963",
+            "1940",
+            "1954"
+        ]
+     },
+     {
+        question:"What Does HTML stand for?",
+        answer:"Hypertext Markup Language",
+        options:[
+            "Hypertext Markup Language",
+            "Hyper Manual Language",
+            "Hyper Makeup Learning",
+            "Keine Antworte"
+        ]
+     },
+     {
+       question:"Which Country Has the best Programmers?",
+       answer:"India",
+       options:[
+           "America",
+           "China",
+           "India",
+           "Russia",
+       ]
+     },
+];
